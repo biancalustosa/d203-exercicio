1755e51ed0315f25898fabbec8fc7dff3833b648
Bianca <biafelix08@gmail.com>
2019-02-13 12:12:42 -0200

    Exercicio-d203

---
+++ script.js
@@ -0,0 +1,28 @@
+const aleatorio = function(min, max){
+    return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
+console.log(aleatorio(0, 50))
+
+prompt('Escolha um número');
+
+let
+
+if(){
+
+}
+
+else{
+
+}
+/*
+O objetivo é criar um jogo de adivinhação de números.
+1- Gerar um número aleatório entre 0 e 50;
+2- Pedir para o usuário adivinhar o número;
+3- Se o usuário acertar, informar que ele ganhou;
+4- Se o usuário errar, informar para ele que ele errou e se o número dito é maior ou menor que o número aleatório;
+5- Se o usuário errar, ele também perde uma vida;
+6- O usuário tem 10 vidas para ganhar o jogo;
+*/
+
+
