da98a8859753a1575a58e4914222e2c266ec0b5e
Bianca Lustosa <biafelix08@gmail.com>
2019-02-17 19:33:01 -0300

    Exercicio-de-adivinhação

--- script.js
+++ script.js
@@ -1,44 +1,40 @@
-// const aleatório = function(){
-//   console.log(aleatorio);
-// }
-// let aleatorio = Math.floor(Math.random() * 50 + 1);
 
-const aleatorio = function  (min, max){
-
+function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let i = 0;
-let vidas = 3;
-let palpite = Number(prompt('Tente adivinhar o número. Você possui 10 tentativas'));
+let numero = aleatorio (0,50);
+let vidas = 9;
 
+while (vidas > 0){
 
-while(vidas>0 && palpite != aleatorio){
-    
+    let palpite = Number(prompt('Tente adivinhar o número aleatório que esta entre 0 e 50. Você tem 10 tentativas.'));
 
-    if(palpite == aleatorio){
+    if(palpite == numero){
+        alert('Parabéns, você venceu o jogo.')
     }
 
     else{
-        alert('Você errou.');
+        alert('Que pena, você errou.')
+        if(palpite > numero){
+            alert(`Você perdeu uma vida, agora possui ${vidas} chances. Está quase lá, seu número é maior do que o número aleatório.`); 
+            // alert(`Está quase lá, seu número é maior do que o número aleatório.`);
+        
+        }
+
+        else{
+            alert(`Você perdeu uma vida, agora possui ${vidas} chances. Está quase lá, seu número é menor do que o número aleatorio.`) 
+            // alert(`Está quase lá, seu número é menor do que o número aleatorio.`);
+            
+        }
+
         vidas --;
     }
-  
-    // if(vidas == 0){
-    //     alert('Sentimos, mas suas vidas acabaram. Você perdeu o jogo.');
-    // }
-    
 
-    if (palpite>aleatorio){
-        alert(`Está quase lá, seu número é maior do que número a ser adivinhado.`);
-        prompt(`Tente novamente. Você possui ${vidas} vidas.`);
-    }
-    
-    
-    else {
-        alert(`Está quase lá, seu número é menor do que número a ser adivinhado.`);
-        prompt(`Tente novamente. Você possui ${vidas} vidas.`);
-    }   
-    
-}     
-   
+    // if(vidas=0){
+    //     alert('Sentimos,mas suas vidas acabaram.');
+    // }
+
+}
+
+
